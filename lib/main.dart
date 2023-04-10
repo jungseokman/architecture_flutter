018b2f312ed7fefebca9a397df1f90bafd554529
jungseokman <seokman.jung@gmail.com>
2023-04-10 15:37:57 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:advicer/application/core/services/theme_service.dart';
+import 'package:advicer/themes/theme.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  return runApp(
+    ChangeNotifierProvider(
+      create: (context) => ThemeService(),
+      child: const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<ThemeService>(
+      builder: (context, themeService, child) {
+        return MaterialApp(
+          themeMode:
+              themeService.isDarkModeOn ? ThemeMode.dark : ThemeMode.light,
+          theme: AppTheme.lightTheme,
+          darkTheme: AppTheme.darkTheme,
+          home: Container(),
+        );
+      },
+    );
+  }
+}
